3f197286d5e50f0ab601873803b638a96505111e
Suoze Yang <41485059+fengcherenxi@users.noreply.github.com>
2022-01-09 14:23:33 +0800

    Delete reverse-words-in-a-string-iii directory

--- reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def reverseWords(self, s: str) -> str:
-        str_list = s.split(" ")
-        b=""
-        for i in range(len(str_list)-1):
-            b+=str_list[i][::-1]+' '
-        b+=str_list[len(str_list)-1][::-1]
-        return b
