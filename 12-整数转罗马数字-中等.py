afcb283406b2d626900f54b1fb66d9be64ab4b63
Suoze Yang <41485059+fengcherenxi@users.noreply.github.com>
2022-01-09 14:32:15 +0800

    Create 12-整数转罗马数字-中等.py

---
+++ 12-整数转罗马数字-中等.py
@@ -0,0 +1,11 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        numerals = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+
+        result = ''
+        for i in range(len(values)):
+            while num >= values[i]:
+                num -= values[i]
+                result +=  numerals[i]
+        return result
