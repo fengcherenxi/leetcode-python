975844a9012b1e2b273759940eb83159032958a0
Suoze Yang <41485059+fengcherenxi@users.noreply.github.com>
2022-01-10 18:20:30 +0800

    Create 079-单词搜索-中等.py

---
+++ 079-单词搜索-中等.py
@@ -0,0 +1,33 @@
+# 关键解题点
+# 使用回溯法结合深度优先遍历在图中进行快速搜索，考虑清楚回溯的结果和时机是这类问题的关键。
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+        def check(i: int, j: int, k: int) -> bool:
+            if board[i][j] != word[k]:
+                return False
+            if k == len(word) - 1:
+                return True
+            
+            visited.add((i, j))
+            result = False
+            for di, dj in directions:
+                newi, newj = i + di, j + dj
+                if 0 <= newi < len(board) and 0 <= newj < len(board[0]):
+                    if (newi, newj) not in visited:
+                        if check(newi, newj, k + 1):
+                            result = True
+                            break
+            
+            visited.remove((i, j))
+            return result
+
+        h, w = len(board), len(board[0])
+        visited = set()
+        for i in range(h):
+            for j in range(w):
+                if check(i, j, 0):
+                    return True
+        
+        return False
